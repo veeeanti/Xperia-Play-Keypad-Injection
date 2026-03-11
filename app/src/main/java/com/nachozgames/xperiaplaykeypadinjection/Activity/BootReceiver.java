c6ee5d20d252b64049821cdb152e40218fd3515d
vee anti <113473346+veeeanti@users.noreply.github.com>
2026-03-11 06:58:57 -0500

    make it run on boot and add workflow as i can't build locally right now

---
+++ app/src/main/java/com/nachozgames/xperiaplaykeypadinjection/Activity/BootReceiver.java
@@ -0,0 +1,21 @@
+package com.nachozgames.xperiaplaykeypadinjection.Activity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class BootReceiver extends BroadcastReceiver {
+    private static final String TAG = "BootReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Log.i(TAG, "Device boot completed, launching application...");
+            
+            Intent launchIntent = new Intent(context, MainActivity.class);
+            launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(launchIntent);
+        }
+    }
+}
